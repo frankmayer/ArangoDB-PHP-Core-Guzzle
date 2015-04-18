978346d04e9e91c3330923ee16c94303d5ca7468
Frank Mayer <frank@frankmayer.net>
2015-04-18 21:33:27 +0300

    Fix travis tests...

---
+++ tests/before.sh
@@ -0,0 +1,15 @@
+#! /bin/bash bash
+#
+# This script is run by Travis CI before the test script.
+# It updates composer
+
+echo -- Updating Composer
+wget http://getcomposer.org/composer.phar
+# php composer.phar require satooshi/php-coveralls:dev-master --dev --no-progress --prefer-source
+
+# composer self-update
+
+# those are not needed
+#
+# echo -- Installing the dependencies
+# composer install --dev
